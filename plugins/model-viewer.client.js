b3ba62116de727e8f8d5964d0db79a69e4739a0b
Mas Muhammad Rizqi Adiguna <rizqiadiguna4@gmail.com>
2025-06-25 16:27:55 +0700

    Fix build errors: resolve three.js dependency and plugin format

--- plugins/model-viewer.client.js
+++ plugins/model-viewer.client.js
@@ -1,1 +1,9 @@
 import '@google/model-viewer';
+// plugins/model-viewer.client.js
+import { defineNuxtPlugin } from '#app';
+import '@google/model-viewer'; // Impor model-viewer di sini
+
+export default defineNuxtPlugin(() => {
+  // Anda bisa menambahkan logika inisialisasi tambahan di sini jika diperlukan,
+  // tetapi untuk sekadar mendaftarkan web component, impor di atas sudah cukup.
+});
