689060aab7a4d5c6b27432e987b49f42551de37a
Mas Muhammad Rizqi Adiguna <rizqiadiguna4@gmail.com>
2025-07-01 06:10:34 +0700

    vsiitor fix pls

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,8 +13,8 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      JSONBIN_API_KEY: process.env.VITE_JSONBIN_API_KEY,
-      JSONBIN_BIN_ID: process.env.VITE_JSONBIN_BIN_ID
+      JSONBIN_API_KEY: process.env.JSONBIN_API_KEY,
+      JSONBIN_BIN_ID: process.env.JSONBIN_BIN_ID
     }
   }
 })
