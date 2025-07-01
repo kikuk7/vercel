301ef194daf6b7c30ad9cbb3fe8c20bc497b23b0
Mas Muhammad Rizqi Adiguna <rizqiadiguna4@gmail.com>
2025-07-01 20:29:26 +0700

    Fix viss

--- components/ContactSection.vue
+++ components/ContactSection.vue
@@ -45,7 +45,7 @@
 
     <div class="map">
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63281.57198165715!2d109.02007107341548!3d-6.895978278010659!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708b1aa9b8c8a1%3A0x3027a76e35212aa6!2sTegal%2C%20Central%20Java%2C%20Indonesia!5e0!3m2!1sen!2sid!4v1685389336040!5m2!1sen!2sid"
+        src="https://www.google.com/maps/place/Sumber+Alam+Grup/@-6.915304,109.1437803,17z/data=!3m1!4b1!4m6!3m5!1s0x2e6fb8fc4e0a830f:0x18d216b7449db15c!8m2!3d-6.915304!4d109.1437803!16s%2Fg%2F11f25l2fmr?entry=ttu&g_ep=EgoyMDI1MDYyNi4wIKXMDSoASAFQAw%3D%3D"
         width="100%" height="250" style="border:0; border-radius: 12px;" allowfullscreen="" loading="lazy">
       </iframe>
     </div>
