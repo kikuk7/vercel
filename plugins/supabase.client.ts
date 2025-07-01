76a05aafb0a5e8516c00361ac3405401dd56f329
Mas Muhammad Rizqi Adiguna <rizqiadiguna4@gmail.com>
2025-07-01 08:17:28 +0700

    Fix visitor lokal final

--- plugins/supabase.client.ts
+++ plugins/supabase.client.ts
@@ -1,21 +1,19 @@
+// plugins/supabase.client.ts
 import { createClient } from '@supabase/supabase-js'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const config = useRuntimeConfig() as unknown as {
-    public: {
-      supabaseUrl: string
-      supabaseAnonKey: string
-    }
+  const config = useRuntimeConfig()
+
+  const supabaseUrl = config.public.supabaseUrl
+  const supabaseAnonKey = config.public.supabaseAnonKey
+
+  if (!supabaseUrl || !supabaseAnonKey) {
+    console.error('❌ Supabase URL:', supabaseUrl)
+    console.error('❌ Supabase Key:', supabaseAnonKey)
+    throw new Error('❌ Supabase URL or Anon Key is missing!')
   }
 
-  if (!config.public.supabaseUrl || !config.public.supabaseAnonKey) {
-    throw new Error('Supabase URL or Anon Key is missing!')
-  }
-
-  const supabase = createClient(
-    config.public.supabaseUrl,
-    config.public.supabaseAnonKey
-  )
+  const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
   return {
     provide: {
